f209cf7dea38402f71e8c056546a0f5c22d9a878
ChoiSou0 <csy050420@gmail.com>
2022-02-12 05:39:32 +0900

    ㅇ

---
+++ Assets/Script/JCW/MenuManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Menu_Back()
+    {
+        Menuwindow menu = GameObject.Find("Dark").GetComponent<Menuwindow>();
+        menu.num = 0;
+    }
+    public void Sound_Back()
+    {
+        SoundWindow Sound = GameObject.Find("SoundWindow").GetComponent<SoundWindow>();
+        Sound.num = 0;
+    }
+    public void Creator_Back()
+    {
+        CreatorWindow Creator = GameObject.Find("CreatorWindow").GetComponent<CreatorWindow>();
+        Creator.num = 0;
+    }
+    public void Title_Back()
+    {
+        TitleWindow Title = GameObject.Find("Title_Dark").GetComponent<TitleWindow>();
+        Title.num = 0;
+    }
+    public void Title_Yes()
+    {
+        SceneManager.LoadScene("Title");
+    }
+    public void Quit_Back()
+    {
+        QuitWindow Main = GameObject.Find("Quit_Dark").GetComponent<QuitWindow>();
+        Main.num = 0;
+    }
+
+    public void Quit_Yes()
+    {
+        Application.Quit();
+    }
+
+    public void Sound()
+    {
+        SoundWindow Sound = GameObject.Find("SoundWindow").GetComponent<SoundWindow>();
+        Sound.num = 1;
+    }
+    public void Creator()
+    {
+        CreatorWindow Creator = GameObject.Find("CreatorWindow").GetComponent<CreatorWindow>();
+        Creator.num = 1;
+    }
+    public void Title()
+    {
+        TitleWindow Title = GameObject.Find("Title_Dark").GetComponent<TitleWindow>();
+        Title.num = 1;
+    }
+    public void Quit()
+    {
+        TitleWindow Title = GameObject.Find("Title_Dark").GetComponent<TitleWindow>();
+        if (Title.num == 0)
+        {
+            QuitWindow Main = GameObject.Find("Quit_Dark").GetComponent<QuitWindow>();
+            Main.num = 1;
+        }
+    }
+}
