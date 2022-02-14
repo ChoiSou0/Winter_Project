7c9bad5199ec97ac42836c679de6a04ec0da0f45
woalsdl04 <woalsdl0439@gmail.com>
2022-02-14 15:33:24 +0900

    인터페이스 제작

--- Assets/Script/JCW/HP_bar.cs
+++ Assets/Script/JCW/HP_bar.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HP_bar : MonoBehaviour
 {
+
+    Slider slider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider = GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Player_Control player = GameObject.Find("Player").GetComponent<Player_Control>();
+        slider.maxValue = player.Player_MaxHp;
+        slider.value = player.Player_Hp;
     }
 }
